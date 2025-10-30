fcc6a6b7ed97209324e732bbcf715b656eb2b27e
Ankit-kumar8-1 <ankitksoftware@gmail.com>
2025-10-30 17:37:55 +0530

    Reset file structure and Create Entity Person

---
+++ src/main/java/com/Springjpa/learnjpamapping/Entities/Person.java
@@ -0,0 +1,13 @@
+package com.Springjpa.learnjpamapping.Entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+
+public class Person {
+    @Id
+
+    private  int id ;
+
+}
